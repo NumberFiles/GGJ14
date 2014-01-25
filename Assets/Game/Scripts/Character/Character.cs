e37ff1557815e5b9ad3ad3b5b572e40284225866
Daniel Kelly <dlpkelly@hotmail.com>
2014-01-25 14:24:13 -0500

    Blue and red shift
    Made it nice and gradual

--- Assets/Game/Scripts/Character/Character.cs
+++ Assets/Game/Scripts/Character/Character.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(CharacterMotor))]
 [RequireComponent(typeof(CharacterController))]
-[RequireComponent(typeof(Light))]
 public class Character : MonoBehaviour {
+	protected CharacterMotor motor;
 	
 	public float healthPoints {
 		get;
@@ -16,41 +17,15 @@ public class Character : MonoBehaviour {
 	
 	
 	public CharacterController characterController;
-	public Light EmittedLight;
-	private Color currentLightColour;
 	
 	// Use this for initialization
-	void Start () {
+	protected virtual void Start () {
 		SetInitialProperties();
 		characterController = GetComponent<CharacterController>();
-		EmittedLight = GetComponent<Light>();
+		motor = GetComponent<CharacterMotor>();
 	}
-	
-	// FixedUpdate is called once per game tick
-	void FixedUpdate () {
-		UpdateLightBasedOnVelocity();
-	}
-	
-	void UpdateLightBasedOnVelocity() {
-		float sqrMagnitude = characterController.velocity.sqrMagnitude;
-		if (sqrMagnitude <= 0.0f) {
-			currentLightColour = Color.red;
-		}
-		else if (sqrMagnitude > 5.0f) {
-			currentLightColour = Color.blue;
-		}
-		EmittedLight.color = currentLightColour;
-	}
-	
 	
 	void SetInitialProperties() {
 		healthPoints = 100;
-	}
-	void MakeRedder() {
-		
-	}
-	
-	void MakeBluer() {
-	
 	}
 }
