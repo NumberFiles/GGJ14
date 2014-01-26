74debd7fa792aa7fa098cf1e7ec67535218c4aa9
Daniel Kelly <dlpkelly@hotmail.com>
2014-01-25 20:13:01 -0500

    weapon selection

--- Assets/Game/Scripts/Character/Player/PlayerInputController.cs
+++ Assets/Game/Scripts/Character/Player/PlayerInputController.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(CharacterMotor))]
 public class PlayerInputController : Controller {
 	
 	// Update is called once per frame
@@ -20,6 +19,27 @@ public class PlayerInputController : Controller {
 			input.fire = true;
 		if(Input.GetButtonUp("Fire1"))
 			input.fire = false;
+		
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+			input.weapon = 0;
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+			input.weapon = 1;
+		if(Input.GetKeyDown(KeyCode.Alpha3))
+			input.weapon = 2;
+		if(Input.GetKeyDown(KeyCode.Alpha4))
+			input.weapon = 3;
+		if(Input.GetKeyDown(KeyCode.Alpha5))
+			input.weapon = 4;
+		if(Input.GetKeyDown(KeyCode.Alpha6))
+			input.weapon = 5;
+		if(Input.GetKeyDown(KeyCode.Alpha7))
+			input.weapon = 6;
+		if(Input.GetKeyDown(KeyCode.Alpha8))
+			input.weapon = 7;
+		if(Input.GetKeyDown(KeyCode.Alpha9))
+			input.weapon = 8;
+		if(Input.GetKeyDown(KeyCode.Alpha0))
+			input.weapon = 9;
 		
 		input.move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 		
