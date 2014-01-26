b41ccfed82ce2bbf31d6c474401121def4ceb466
Patrick Boyer <boyer.patrick@gmail.com>
2014-01-25 21:52:25 -0500

    Removed merge results from the source file

--- Assets/Game/Scripts/Character/Character.cs
+++ Assets/Game/Scripts/Character/Character.cs
@@ -5,15 +5,12 @@ using System.Collections.Generic;
 [RequireComponent(typeof(CharacterMotor))]
 [RequireComponent(typeof(CharacterController))]
 public class Character : MonoBehaviour {
-<<<<<<< HEAD
+
 	protected CharacterMotor motor;
-=======
 	protected Controller control;
-	protected CharacterMotor motor;
 	
 	public string[] weaponSlots = { "Pistol", "Rocket" };
 	protected Dictionary<string, Weapon> slottedWeapons = new Dictionary<string, Weapon>();
->>>>>>> origin/master
 	
 	public float healthPoints {
 		get;
@@ -23,12 +20,9 @@ public class Character : MonoBehaviour {
 	public bool IsDead {
 		get { return healthPoints <= 0.0f; }
 	}
-	
-	
+		
 	public CharacterController characterController;
-	
-<<<<<<< HEAD
-=======
+
 	protected virtual void FixedUpdate() {
 		Weapon[] weapons = GetComponentsInChildren<Weapon>();
 		slottedWeapons.Clear();
@@ -46,15 +40,11 @@ public class Character : MonoBehaviour {
 		}
 	}
 	
->>>>>>> origin/master
 	// Use this for initialization
 	protected virtual void Start () {
 		SetInitialProperties();
 		characterController = GetComponent<CharacterController>();
-<<<<<<< HEAD
-=======
 		control = GetComponent<Controller>();
->>>>>>> origin/master
 		motor = GetComponent<CharacterMotor>();
 	}
 	
