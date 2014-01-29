603e722ab034fcc390b94dc7a3ac93e0b7fe1f8f
Daniel Kelly <dlpkelly@hotmail.com>
2014-01-29 18:46:37 -0500

    rebalance and quick fix to avoid vertical momentum transfer

--- Assets/Game/Scripts/Character/CharacterMotor.cs
+++ Assets/Game/Scripts/Character/CharacterMotor.cs
@@ -17,28 +17,28 @@ public class CharacterMotor : MonoBehaviour {
 	
 	[System.Serializable]
 	public class Movement {
-		public float runSpeed = 8.0f; //Running speed in units per second
-		public float sqrunSpeed { get { return runSpeed * runSpeed; } }
-		public float groundFriction = 40.0f; //Deceleration of grounded player going above run speed in units per second per second
+		public float runSpeed = 10.0f; //Running speed in units per second
+		public float sqrRunSpeed { get { return runSpeed * runSpeed; } }
+		public float groundFriction = 45.0f; //Deceleration of grounded player going above run speed in units per second per second
 		public float drag = 1 / 500.0f; //Players drag in the air. Multiply by 1/2 velocity squared to get deceleration
-		public float airAcceleration = 2.0f; //Acceleration of player in the air
-		public float gravity = 10.5f;
+		public float airAcceleration = 2.5f; //Acceleration of player in the air
+		public float gravity = 14f;
 	}
 	public Movement move = new Movement();
 	
 	[System.Serializable]
 	public class Sliding {
-		public float slidingSpeed = 8.1f; //Running speed in units per second
+		public float slidingSpeed = 12.1f; //Running speed in units per second
 		public float sqrSlidingSpeed { get { return slidingSpeed * slidingSpeed; } }
-		public float stopRate = 5.0f; //Rate of deceleration when player opposes movement above sliding speed in units per second per second
+		public float stopRate = 7.0f; //Rate of deceleration when player opposes movement above sliding speed in units per second per second
 	}
 	public Sliding slide = new Sliding();
 	
 	[System.Serializable]
 	public class Jumping {
-		public float verticalJumpImpulse = 6.0f; //Players vertical change in speed when jumping up in units per second
+		public float verticalJumpImpulse = 8.0f; //Players vertical change in speed when jumping up in units per second
 		public float directionalJumpImpulse = 3f; //Players horizontal change in speed when jumping in a direction in units per second
-		public float directionalJumpVerticalImpulse = 4.2f; //Players vertical change in speed when jumping in a direction in units per second
+		public float directionalJumpVerticalImpulse = 5.6f; //Players vertical change in speed when jumping in a direction in units per second
 		public float earlyJumpPenalty = 0.1f; //Players horizontal loss of speed for jumping early as a fraction
 	}
 	public Jumping jump = new Jumping();
@@ -46,9 +46,9 @@ public class CharacterMotor : MonoBehaviour {
 	[System.Serializable]
 	public class WallRunning {
 		public float range = 0.2f; //Minimum ratio of gravity that must be applied when wallrunning and rising
-		public float minRisingGravity = 0.5f; //Minimum ratio of gravity that must be applied when wallrunning and rising
+		public float minRisingGravity = 0.75f; //Minimum ratio of gravity that must be applied when wallrunning and rising
 		public float risingGravityReductionSlope = 0.075f; //Rate of gravity reduction increase
-		public float minFallingGravity = 0.25f;  //Minimum ratio of gravity that must be applied when wallrunning and falling
+		public float minFallingGravity = 0.5f;  //Minimum ratio of gravity that must be applied when wallrunning and falling
 		public float fallingGravityReductionSlope = 0.075f; //Minimum ratio of gravity that must be applied when wallrunning and rising
 		public float friction = 0.0f; //Deceleration of grounded player going above run speed in units per second per second
 		public float momentumTransfer = 0.75f;
@@ -123,7 +123,7 @@ public class CharacterMotor : MonoBehaviour {
 					
 					if(!isWallRunning) {
 						float sideSpeed = Vector3.Dot(hVel, side);
-						Vector3 forward = velocity - sideSpeed * side;
+						Vector3 forward = hVel - sideSpeed * side;
 						velocity += forward.normalized * sideSpeed *wallrun.momentumTransfer;
 					}
 					
